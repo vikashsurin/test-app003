57abec68221b906b7701c5f1c4017d49bff5155a
vikashsurin <vikashsurin10@gmail.com>
2019-07-24 22:13:49 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express'); //common js only
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
+});
