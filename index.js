f5410ad71e9b50286b02fd15cbae8054ab74d7be
vikashsurin <vikashsurin10@gmail.com>
2019-07-31 13:44:45 +0530

    added production setup

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express'); //common js only
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+
+mongoose.connection
+  .on('error', console.error.bind(console, 'connection error'))
+  .once('open', () => {
+    console.log('connection established to mongoDB');
+  });
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
